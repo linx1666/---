bc6f20371521a390bf7c664255e9dd34b004bcf5
linx1666 <3152704030@qq.com>
2025-03-17 23:22:54 +0800

    demo-2

--- guess.c
+++ guess.c
@@ -3,25 +3,37 @@
 #include<time.h>
 
 int main(){
-	int number,guess,count=1,i;
+	int number,guess,count=0,i,yorn;
 	srand((unsigned int)time(NULL));
 	number=rand();
 
-	printf("guess it!");
+	printf("guess it!\n");
 	
-	for(i=1; ;i=i+1){
+	for(i=1; ;i=i+1)
+	{
 	scanf("%d",&guess);
-	if(count>=10){
-		break;
-	} 
-	if(number==guess){
-		printf("yes!you are right!"); 
-		break;
-	}
+
+	if(number<guess){
+		printf("Try a smaller number\n"); 
+		}
+	else if(number>guess){
+		printf("Try a bigger number\n"); 
+		}
 	else{
-		printf("nonono"); 
+		printf("Yes you are right! Good job!");
+		}
+
 		count=count+1;
+
+		if(count>15){
+			printf("Do you want try again?\n");
+			scanf("%d",&yorn);
+			if(yorn==0){
+				break;
+			}
+		}
 	}
-}
-printf("haha,it is %d",number); 
+
+	printf("The number is %d\n",number);
+	printf("You counted %d times",count);
 } 
